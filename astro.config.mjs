c74818afb330e052210bcfe6b5876abc79ca4124
Emilia Laviero <lavieroemilia@gmail.com>
2026-06-04 16:42:00 +0200

    fix: config base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://dino-design-system-v2.vercel.app',
+  base: '/', // Esto obliga a Astro a usar rutas desde la raíz del dominio
 });
